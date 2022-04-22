4eaccf46861e80d2bf2af23cf06636f8981968bd
doraSwiper <dora0swiper@gmail.com>
2022-04-23 06:54:59 +0700

    menambahkan judul website dan hamburger line

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ['./public/**/*.{html, js}'],
   theme: {
     extend: {},
+    colors : {
+      primary: '#2dd4bf',
+      dark: '#1f2937'
+    }
   },
   plugins: [],
 }
