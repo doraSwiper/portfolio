5707876c7d6a41b4c84cfba15b1cc7554e56e295
doraSwiper <dora0swiper@gmail.com>
2022-04-23 19:50:04 +0700

    menambahkan fitur navbar : finished-header

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,23 @@
 
-// Hamburger on Mobile
+// Hamburger on Mobile && nav on mobile
 const hamburger = document.getElementById('hamburger');
+let nav = document.querySelector('#nav');
 
 hamburger.onclick = (evt) => {
-   hamburger.classList.toggle('hamburger-active')
+   hamburger.classList.toggle('hamburger-active');
+   if(nav.classList[0] === 'hidden'){
+      nav.classList.remove('hidden');
+      nav.classList.add('block');
+   }else {
+      nav.classList.remove(nav.classList[0]);
+      nav.classList.add('hidden');
+   }
+}
+
+// Nacbar Scroll
+
+window.onscroll = () => {
+   const header = document.querySelector('header');
+   const fixedNav = header.offsetTop;
+   (window.pageYOffset > fixedNav) ? header.classList.add('navbar-fixed') : header.classList.remove('navbar-fixed');
 }
