bda959369678ca7a1ac5cb10cd41f782bdc8e2ce
doraSwiper <dora0swiper@gmail.com>
2022-04-29 20:38:39 +0700

    menambahkan fitur -navbar-body-close

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
 
 // Hamburger on Mobile && nav on mobile
 const hamburger = document.getElementById('hamburger');
-const span2 = document.querySelector('#hamburger span:nth-child(2)')
+const span2 = document.querySelector('#hamburger > span:nth-child(2)')
 let nav = document.querySelector('#nav');
+const bodyClose = document.querySelector('#body-close');
 
 hamburger.onclick = (evt) => {
    hamburger.classList.toggle('hamburger-active');
@@ -11,11 +12,26 @@ hamburger.onclick = (evt) => {
       if(val !== 'hidden') {
           nav.classList.add('block');
           span2.setAttribute('class', 'hamburger-line hamburger-active transition');
+         bodyClose.classList.remove('hidden');
       } else{
           nav.classList.remove('block');
-          span2.removeAttribute('class', 'hamburger-line hamburger-active transition')
+          span2.removeAttribute('class', 'hamburger-line hamburger-active transition');
+          bodyClose.classList.add('hidden')
       }
    });
+}
+
+// bodyClose
+bodyClose.onclick = (evt) => {
+   if(nav.classList.contains('block')){
+      nav.classList.remove('block');
+      nav.classList.add('hidden');
+      if(span2.classList.contains('hamburger-line', 'hamburger-active', 'transition') && hamburger.classList.contains('hamburger-active')){
+         span2.removeAttribute('class', 'hamburger-line hamburger-active transition');
+         hamburger.classList.remove('hamburger-active')
+      }
+      bodyClose.classList.add('hidden');
+   }
 }
 
 // Nacbar Scroll
